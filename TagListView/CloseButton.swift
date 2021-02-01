b79fc702ba20e86e503f6234b3bcbc1ba7238210
soroush-at <74778232+soroush-at@users.noreply.github.com>
2021-02-01 18:08:46 +0330

    Made removeButton accessable and customizable.

--- TagListView/CloseButton.swift
+++ TagListView/CloseButton.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-internal class CloseButton: UIButton {
+open class CloseButton: UIButton {
 
     var iconSize: CGFloat = 10
     var lineWidth: CGFloat = 1
     var lineColor: UIColor = UIColor.white.withAlphaComponent(0.54)
 
-    weak var tagView: TagView?
-
-    override func draw(_ rect: CGRect) {
+    public weak var tagView: TagView?
+    
+    init() {
+        super.init(frame: .zero)
+        if #available(iOS 13.0, *) {
+            setImage(UIImage(systemName: "xmark")!, for: .normal)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func draw(_ rect: CGRect) {
+        if #available(iOS 13.0, *) {
+            return
+        }
         let path = UIBezierPath()
 
         path.lineWidth = lineWidth
